36273e889e28dad03a3a5267e2cf2c2e6c99b451
Vlad Khotynskiy <vlhotinskiy@gmail.com>
2024-01-30 23:17:20 +0200

    now it's possible to get,post, delete product (used rtk-query)

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import './App.css'
+import { useState } from 'react'
+import { useGetGoodsQuery, useAddProductMutation, useDeleteProductMutation } from "./redux"
+
+function App() {
+const [count, setCount] = useState('')
+const [newProduct, setNewProduct] = useState('')
+const {data = [], error, isError, isLoading} = useGetGoodsQuery(count)
+const [addProduct, {}] = useAddProductMutation()
+const [deleteProduct] = useDeleteProductMutation()
+
+const handleAddProduct = async () => {
+  if (newProduct) {
+    await addProduct({name: newProduct}).unwrap()
+    setNewProduct('')
+  }
+}
+
+const handleDeleteProduct = async (id) => {
+  await deleteProduct(id).unwrap()
+}
+
+if (isLoading) return <h1>Loading...</h1>
+if (isError) return <h1>{error.error}</h1>
+
+  return (
+    <div className="App">
+      <div>
+        <input type="text" value={newProduct} onChange={(e) => setNewProduct(e.target.value)}/>
+        <button onClick={handleAddProduct}>Add product</button>
+      </div>
+      <div>
+        <select value={count} onChange={(e) => setCount(e.target.value)}>
+          <option value="''">all</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+      </div>
+      <ul>
+        {data.map(item => (
+          <li key={item.id} onClick={() => handleDeleteProduct(item.id)}>
+            {item.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
